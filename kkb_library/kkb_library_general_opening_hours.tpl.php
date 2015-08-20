31dbc00a4ba7aa277dc71b304992e37aadf44b7f
Troels Lenda <troelslenda@gmail.com>
2015-08-20 11:52:06 +0200

    Removing title as it is now part of the image instead.

--- kkb_library/kkb_library_general_opening_hours.tpl.php
+++ kkb_library/kkb_library_general_opening_hours.tpl.php
@@ -1,5 +1,10 @@
+<?php
+/**
+ * @file
+ * Template for general library opening hours.
+ */
+?>
 <div class="library-opening-hours">
-<h1><?php print t('Biblioteket online'); ?></h1>
 <div class="text"><?php print $text['value']; ?></div>
 <div class="readmore-container"><?php print $button; ?></div>
 </div>
