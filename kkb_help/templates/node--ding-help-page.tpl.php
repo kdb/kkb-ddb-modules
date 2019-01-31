a097852e1efc17e18a5cc8aa95af4e8ef62fb0ea
Thomas Fini Hansen <xen@xen.dk>
2019-01-31 14:32:00 +0100

    Started theming
    Ref FAQ-14

---
+++ kkb_help/templates/node--ding-help-page.tpl.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @file
+ * Custom node template for help pages.
+ */
+?>
+<article class="<?php print $classes; ?>"<?php print $attributes; ?>>
+
+  <h1 class="page-title"><?php print $title; ?></h1>
+
+
+  <?php
+    // Hide fields.
+    hide($content['comments']);
+    hide($content['links']);
+    hide($content['field_ding_help_page_related']);
+    ?>
+  <section class="ding-help-page-main">
+    <?php print render($content); ?>
+  </section>
+
+  <aside class="ding-help-page-sidebar">
+    <?php print render($content['field_ding_help_page_related']); ?>
+  </aside>
+</article>
