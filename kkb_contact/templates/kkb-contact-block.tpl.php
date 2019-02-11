c94e4278882fd6e068f6442731b1ec95d1a55d3b
Thomas Fini Hansen <xen@xen.dk>
2019-02-11 12:45:14 +0100

    Add contact footer

---
+++ kkb_contact/templates/kkb-contact-block.tpl.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @file
+ * Template for contact block.
+ */
+?>
+<section class="phone-section">
+  <h3>Ring til biblioteket online:</h3>
+  <?php print $phone ?>
+</section>
+<section>
+  <h3>Telefontid:</h3>
+  <?php print $opening_hours ?>
+</section>
+<section>
+  <h3><a href="/kontakt#skriv">Skriv</a></h3>
+</section>
