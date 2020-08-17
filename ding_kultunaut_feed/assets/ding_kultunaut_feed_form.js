bf1f3bde3128f77d6fcd209cf745d1be7b7dc599
Carsten Høyer <carsten@reload.dk>
2020-08-17 15:04:55 +0200

    Use form validation for ding_event
    KKB-346

--- ding_kultunaut_feed/assets/ding_kultunaut_feed_form.js
+++ ding_kultunaut_feed/assets/ding_kultunaut_feed_form.js
@@ -15,20 +15,6 @@
             var showToDateCheckbox = document.querySelector('#edit-field-exposed-dates-und-0-show-todate');
             var endDates = document.querySelector('.form-item-field-exposed-dates-und-0-value2');
             endDates.style.display = showToDateCheckbox.checked ? 'inherit' : 'none';
-
-            // If the "special exposed event" checkbox is not checked,
-            // we need to reset the date group fields to avoid validation errors.
-            // We only do this on submit, because we want to keep the values if the
-            // user unchecks and checks the "special exposed event" checkbox.
-            var submit = document.querySelector('#edit-submit');
-            submit.addEventListener('click', function() {
-                if (!checkbox.checked) {
-                    var inputs = Array.from(dates.querySelectorAll('input[type=text]'));
-                    inputs.map(function(input) {
-                        input.value = '';
-                    });
-                }
-            });
         }
     };
 })();
