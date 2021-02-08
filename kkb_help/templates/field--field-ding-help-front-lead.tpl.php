5454682d68bdef241280434df4efb66c1c4b2c00
Carsten Høyer <carsten@reload.dk>
2021-02-08 11:46:51 +0100

    Fix language issue in cludo search

--- kkb_help/templates/field--field-ding-help-front-lead.tpl.php
+++ kkb_help/templates/field--field-ding-help-front-lead.tpl.php
@@ -9,38 +9,40 @@
   </div>
 </div>
 
+<?php if($element['#object']->language == 'da'): ?>
   <form id="cludo-search-form" role="search">
     <input type="search" class="search-input" aria-label="Search" aria-describedby="autocomplete_hint" placeholder="Hvad søger du hjælp til?">
     <button type="submit" class="search-button" id="search-button">Søg</button>
   </form>
 
-<div id="cludo-search-results">
-  <div class="cludo-r">
-    <div class="cludo-c-3">
-      <div class="search-filters" role="navigation"></div>
-    </div>
-    <div class="cludo-c-9" role="main">
-      <div class="search-result-count"></div>
-      <div class="search-did-you-mean"></div>
-      <div class="search-results"></div>
+  <div id="cludo-search-results">
+    <div class="cludo-r">
+      <div class="cludo-c-3">
+        <div class="search-filters" role="navigation"></div>
+      </div>
+      <div class="cludo-c-9" role="main">
+        <div class="search-result-count"></div>
+        <div class="search-did-you-mean"></div>
+        <div class="search-results"></div>
+      </div>
     </div>
   </div>
-</div>
 <?php else: ?>
-<form id="cludo-search-form-english" role="search">
-  <input type="search" class="search-input" aria-label="Search" aria-describedby="autocomplete_hint" placeholder="What are you looking for help with?">
-  <button type="submit" class="search-button" id="search-button">Search</button>
-</form>
+  <form id="cludo-search-form-english" role="search">
+    <input type="search" class="search-input" aria-label="Search" aria-describedby="autocomplete_hint" placeholder="What are you looking for help with?">
+    <button type="submit" class="search-button" id="search-button">Search</button>
+  </form>
 
-<div id="cludo-search-results">
-  <div class="cludo-r">
-    <div class="cludo-c-3">
-      <div class="search-filters" role="navigation"></div>
-    </div>
-    <div class="cludo-c-9" role="main">
-      <div class="search-result-count"></div>
-      <div class="search-did-you-mean"></div>
-      <div class="search-results"></div>
+  <div id="cludo-search-results">
+    <div class="cludo-r">
+      <div class="cludo-c-3">
+        <div class="search-filters" role="navigation"></div>
+      </div>
+      <div class="cludo-c-9" role="main">
+        <div class="search-result-count"></div>
+        <div class="search-did-you-mean"></div>
+        <div class="search-results"></div>
+      </div>
     </div>
   </div>
-</div>
+<?php endif; ?>
